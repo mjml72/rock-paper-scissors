15900b920cb0ddf7310726adf8aaf5a660607bd3
Miguel Jesús <mjml1972@outlook.es>
2023-09-06 16:26:06 +0100

    mejoras visuales

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const userScoreDisplay = document.getElementById("user-score");
-const compScoreDisplay = document.getElementById("computer-score");
+const computerScoreDisplay = document.getElementById("computer-score");
 const userChoiceDisplay = document.getElementById("user-choice");
-const compChoiceDisplay = document.getElementById("comp-choice");
+const computerChoiceDisplay = document.getElementById("comp-choice");
 const result = document.getElementById("result");
 const btns = document.querySelectorAll(".btn");
 const reset = document.getElementById("reset")
 let userScore = 0;
 let compScore = 0;
-let userChoice, compChoice;
+let userChoice, computerChoice;
 const choices = ["rock", "paper", "scissors"];
 
 reset.addEventListener("click", ()=>{
-    window.location.reload(true);
+    location.reload(true);
 });
 
 
 btns.forEach(element => {
     element.addEventListener("click", (event)=>{
         userChoice = event.target.id;
-        userChoiceDisplay.textContent = `Your choice: ${userChoice.toUpperCase()}`;
+        userChoiceDisplay.textContent = `${userChoice.toUpperCase()}`;
         game(userChoice);
     })
 });
 
-function generateCompChoice() {
-    compChoice = choices[Math.floor(Math.random() * 3)];
-    compChoiceDisplay.textContent = `Comp choice: ${compChoice.toUpperCase()}`;
-    return compChoice;
+function generateCpuChoice() {
+    computerChoice = choices[Math.floor(Math.random() * 3)];
+    computerChoiceDisplay.textContent = `${computerChoice.toUpperCase()}`;
+    return computerChoice;
 }
 
 function game(userChoice) {
-    compChoice = generateCompChoice();
+    computerChoice = generateCpuChoice();
     
-    switch(userChoice + compChoice){
+    switch(userChoice + computerChoice){
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
@@ -45,7 +45,7 @@ function game(userChoice) {
         case "scissorsrock":
            result.textContent = " You Lose!";
            compScore ++;
-           compScoreDisplay.textContent = compScore;
+           computerScoreDisplay.textContent = compScore;
            break;
         case "rockrock":
         case "paperpaper":
